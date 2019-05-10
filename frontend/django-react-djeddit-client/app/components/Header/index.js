bbaf09e28690fdcb8bad1b49ffe62a0e8318903c
LennyLip <vermus.jabber@gmail.com>
2019-05-10 19:11:30 +0300

    header + navbar

--- frontend/django-react-djeddit-client/app/components/Header/index.js
+++ frontend/django-react-djeddit-client/app/components/Header/index.js
@@ -1,27 +1,61 @@
 import React from 'react'
-import { FormattedMessage } from 'react-intl'
+// import { FormattedMessage } from 'react-intl'
+import { Input, Menu } from 'semantic-ui-react'
 
 import A from './A'
 import Img from './Img'
-import NavBar from './NavBar'
-import HeaderLink from './HeaderLink'
+// import NavBar from './NavBar'
+// import HeaderLink from './HeaderLink'
 import Banner from './banner.jpg'
 import messages from './messages'
 
 function Header() {
   return (
     <div>
-      <A href="https://www.reactboilerplate.com/">
-        <Img src={Banner} alt="react-boilerplate - Logo" />
+      <A href="/">
+        <Img src={Banner} alt="django-react-djeddit-client - Logo" />
       </A>
-      <NavBar>
-        <HeaderLink to="/">
-          <FormattedMessage {...messages.home} />
-        </HeaderLink>
-        <HeaderLink to="/features">
-          <FormattedMessage {...messages.features} />
-        </HeaderLink>
-      </NavBar>
+      <Menu secondary>
+        <Menu.Item
+          name="home"
+          active={true}
+          // active={activeItem === 'home'}
+          // onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="topics"
+          // active={activeItem === 'messages'}
+          // onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="friends"
+          // active={activeItem === 'friends'}
+          // onClick={this.handleItemClick}
+        />
+        <Menu.Menu position="right">
+          {/*<Menu.Item>*/}
+            {/*<Input icon="search" placeholder="Search..." />*/}
+          {/*</Menu.Item>*/}
+          <Menu.Item
+            name="Login"
+            // active={activeItem === 'logout'}
+            // onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="Sign Up"
+            // active={activeItem === 'logout'}
+            // onClick={this.handleItemClick}
+          />
+        </Menu.Menu>
+      </Menu>
+      {/*<NavBar>*/}
+      {/*<HeaderLink to="/">*/}
+      {/*<FormattedMessage {...messages.home} />*/}
+      {/*</HeaderLink>*/}
+      {/*<HeaderLink to="/features">*/}
+      {/*<FormattedMessage {...messages.features} />*/}
+      {/*</HeaderLink>*/}
+      {/*</NavBar>*/}
     </div>
   )
 }
--- frontend/django-react-djeddit-client/app/containers/App/index.js
+++ frontend/django-react-djeddit-client/app/containers/App/index.js
@@ -11,6 +11,8 @@ import { Helmet } from 'react-helmet'
 import styled from 'styled-components'
 import { Switch, Route } from 'react-router-dom'
 
+import 'semantic-ui-css/semantic.min.css'
+
 import HomePage from 'containers/HomePage/Loadable'
 import FeaturePage from 'containers/FeaturePage/Loadable'
 import NotFoundPage from 'containers/NotFoundPage/Loadable'
