3d2f2a85eeb908a8869fc6aace390e3e66d75685
LennyLip <vermus.jabber@gmail.com>
2019-05-17 09:56:56 +0300

    fix settings

--- djeddit/urls_api.py
+++ djeddit/urls_api.py
@@ -9,7 +9,7 @@ router = routers.DefaultRouter()
 router.register(r'threads', ThreadViewSet, base_name='threads')
 router.register(r'posts', PostViewSet, base_name='posts')
 
-if settings.DJEDDIT_USE_INTERNAL_USER:
+if getattr(settings, 'DJEDDIT_USE_INTERNAL_USER', None):
     from .apis import UserViewSet
     router.register(r'users', UserViewSet, base_name='users')
 
