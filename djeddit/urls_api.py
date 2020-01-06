a9ce8e1c220fe7de725851a47b5bbbd937483ad6
LennyLip <vermus.jabber@gmail.com>
2020-01-06 22:49:56 +0300

    bump drf

--- djeddit/urls_api.py
+++ djeddit/urls_api.py
@@ -6,13 +6,18 @@ from .apis import ThreadViewSet, PostViewSet, TopicsViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r'threads', ThreadViewSet, base_name='threads')
-router.register(r'posts', PostViewSet, base_name='posts')
-router.register(r'topics', TopicsViewSet, base_name='topics')
+# router.register(r'threads', ThreadViewSet, base_name='threads')
+# router.register(r'posts', PostViewSet, base_name='posts')
+# router.register(r'topics', TopicsViewSet, base_name='topics')
+router.register(r'threads', ThreadViewSet)
+router.register(r'posts', PostViewSet)
+router.register(r'topics', TopicsViewSet)
+
 
 if getattr(settings, 'DJEDDIT_USE_INTERNAL_USER', None):
     from .apis import UserViewSet
-    router.register(r'users', UserViewSet, base_name='users')
+    # router.register(r'users', UserViewSet, base_name='users')
+    router.register(r'users', UserViewSet)
 
 # todo import users api if embed
 
